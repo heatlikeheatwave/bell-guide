017d26356292ef8ac268619f72f4f4224aca8ba3
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2020-04-20 19:54:32 -0400

    Added helpers directory, and started formatData helper function

---
+++ src/helpers/formatData.js
@@ -0,0 +1,9 @@
+import fish from './data/fish.json';
+import bugs from './data/bugs.json';
+
+const formatData = (string) => {
+  string.split("\n");
+  console.log(string);
+}
+
+export default formatData;
