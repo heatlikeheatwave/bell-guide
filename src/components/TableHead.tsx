a6ae9bf5e393b60fa232965801d4e2adda970a72
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2020-04-21 19:01:18 -0400

    Commented out map to extract column names for TableHead component

--- src/components/TableHead.tsx
+++ src/components/TableHead.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const TableHead = (props) => {
+const TableHead = () => {
   return(
     <thead>
       <tr>
         <th>
           {
-          props.map(prop => {
-            return <button
-            type="button"
-            // onClick={() => requestSort('id')}
-            // className={getClassNamesFor('id')}
-            >
-              {}
-            </button>
-          })
+          // props.map(prop => {
+          //   return <button
+          //   type="button"
+          //   // onClick={() => requestSort('id')}
+          //   // className={getClassNamesFor('id')}
+          //   >
+
+          //   </button>
+          // })
           }
         </th>
     </tr>
