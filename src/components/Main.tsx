e8a88dceed5d2a87be0abd4fdb4d0bd3cb77133e
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2020-04-21 18:58:07 -0400

    imported fish and bug data to Main component; wrapped table component in jsx fragments

---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Table from './Table';
+import fish from '../data/fish.json';
+import bugs from '../data/bugs.json';
+
+
+const Main = () => {
+  return(
+    <>
+      <Table/>
+    </>
+  );
+};
+
+export default Main;
